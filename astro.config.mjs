a610ca649760731cf35cca37b9bb5456d08751bb
Yue-plus <Yue_plus@foxmail.com>
2025-03-27 16:32:43 +0800

    feat: pnpm astro add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://yue.zone',
+  site: 'https://yue.zone',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
